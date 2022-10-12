a7b044fd577bfc0b089155770bc49ce6520b0115
carlosalfarodev <rgkalin@gmail.com>
2022-10-11 17:43:06 -0700

    adds react-navigation

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
+import React, { useState } from "react";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>
+        Let's go mimizZz
+        </Text>
+      <Button
+        title="Get Started"
+        onPress={() => null}
+      />
       <StatusBar style="auto" />
     </View>
   );
